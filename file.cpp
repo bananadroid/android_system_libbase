c213d5f7685a65ac3ca40f85ba1b4268d4682984
Frederick Mayle <fmayle@google.com>
2023-01-06 15:38:21 -0800

    Increase ReadFdToString buffer size
    Reduces syscall overhead for large files. For 4k+ files, this can
    improve performance 2x. For smaller files, it seems neutral.
    debuggerd_benchmark showed 4% improvement, but it was a bit noisy, so
    not high confidence.
    
    Test: TH
    Change-Id: Ibc4ddc4de0431926e161af9061f61023e0d1f21e

--- file.cpp
+++ file.cpp
@@ -226,7 +226,8 @@ bool ReadFdToString(borrowed_fd fd, std::string* content) {
     content->reserve(sb.st_size);
   }
 
-  char buf[BUFSIZ] __attribute__((__uninitialized__));
+  // Align to min page size for a minor perf improvement.
+  alignas(4096) char buf[4096] __attribute__((__uninitialized__));
   ssize_t n;
   while ((n = TEMP_FAILURE_RETRY(read(fd.get(), &buf[0], sizeof(buf)))) > 0) {
     content->append(buf, n);
